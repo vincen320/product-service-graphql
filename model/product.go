30389afca6c94d4c9a0ee2c771d5cda11ae321c7
Vincen <vincen320@gmail.com>
2023-05-13 01:31:00 +0700

    add type attr for product interface

--- model/product.go
+++ model/product.go
@@ -2,6 +2,11 @@ package model
 
 import "time"
 
+const (
+	ProductTypeCloth   = 1
+	ProdcutTypeVehicle = 2
+)
+
 type (
 	Product struct {
 		ID          int64     `json:"id"`
@@ -10,5 +15,6 @@ type (
 		Price       int64     `json:"price"`
 		CreatedBy   int64     `json:"created_by"`
 		CreatedAt   time.Time `json:"created_at"`
+		Type        int       `json:"type"`
 	}
 )
